ad5051036583d3c13e4bb933afd10aa37c308cc4
Murilo Lima Matioli <murilo.matioli@pmisa.net>
2024-02-07 09:19:43 -0300

    Exercícios 2 agoora exibe lista com pares

--- 02_Ex_Vetor/main.py
+++ 02_Ex_Vetor/main.py
@@ -1,8 +1,15 @@
 lista = []
+listaPar = []
 tam = int(input("Informe o tamanho da lista: "))
 
-for i in range(1, tam + 1):
-    n = int(input(f"Informe o número [{i}]: "))
+for i in range(tam):
+    n = int(input(f"Informe o número [{i + 1}]: "))
     lista.append(n)
 
-print(lista)
+for i in range(tam):
+    if lista[i] % 2 == 0:
+        listaPar.append(lista[i])
+
+print(f"Lista 1: {lista}")
+print(f"Lista 2: {listaPar}")
+
